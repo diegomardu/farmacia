b1513e167e6f12863f5242624d32ebedc7bcafce
Diego Martins Duarte <diegomartins836@gmail.com>
2019-06-05 13:29:07 -0300

    Criado classe remedio

---
+++ Remedio.py
@@ -0,0 +1,20 @@
+class Remedio:
+    def __init__(self,nome,preco,quantidade):
+        self._nome = nome
+        self._preco = preco
+        self._quantidade = quantidade
+
+    def getNome(self):
+        return self._nome
+    def setNome(self,nome):
+        self._nome = nome
+
+    def getPreco(self):
+        return self._preco
+    def setPreco(self,preco):
+        self._preco = preco
+
+    def getQTD(self):
+        return self._quantidade
+    def setQTD(self,quantidade):
+        self._quantidade = quantidade
